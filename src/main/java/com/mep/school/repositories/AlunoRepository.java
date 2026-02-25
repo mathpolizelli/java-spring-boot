524dacdb9d849bcf1a745618460a30025b23850b
unknown <matheuspolizelli@gmail.com>
2026-02-24 22:00:41 -0300

    service criada

--- src/main/java/com/mep/school/repositories/AlunoRepository.java
+++ src/main/java/com/mep/school/repositories/AlunoRepository.java
@@ -3,5 +3,5 @@ package com.mep.school.repositories;
 import com.mep.school.models.AlunoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlunoInterace extends JpaRepository<AlunoModel, Long> {
+public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
 }
---
+++ src/main/java/com/mep/school/services/AlunoService.java
@@ -0,0 +1,28 @@
+package com.mep.school.services;
+
+import com.mep.school.models.AlunoModel;
+import com.mep.school.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public AlunoModel criarAluno(AlunoModel alunoModel) {
+        return alunoRepository.save(alunoModel);
+    }
+
+    public List<AlunoModel> findAll() {
+        return alunoRepository.findAll();
+    }
+
+    public void deletarAluno(Long id) {
+        alunoRepository.deleteById(id);
+    }
+
+}
